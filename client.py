763d95ff233be49f96d944fe99a5ff0ec9a5a679
Leone, Mark A [Axios] <mark.leone@caci.com>
2022-12-01 11:45:22 -0500

    Working with requests lib and API key read from env file

--- client.py
+++ client.py
@@ -1,35 +1,42 @@
-# import requests
-
-# API_KEY = "SECRET"
-# url = "https://microsoft-translator-text.p.rapidapi.com/languages"
-# querystring = {'api-version':'3.0'}
-# headers = {
-#     'X-RapidAPI-Key': API_KEY,
-#     'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
-#     'accept': 'application/json',
-#     'accept-encoding': 'gzip, deflate, br',
-#     'usequerystring': 'true'
-# }
-# resp = requests.request('GET', url, params=querystring, headers=headers)
-# print(resp.encoding)
-# d = resp.content.decode('utf-8')
-# print(d)
-# print(resp.text)
-
-
-import http.client
+import requests
+import os
 import json
+from dotenv import load_dotenv
 
-conn = http.client.HTTPSConnection("microsoft-translator-text.p.rapidapi.com")
+load_dotenv()
 
+API_KEY = os.environ["API_KEY"]
+url = "https://microsoft-translator-text.p.rapidapi.com/languages"
+querystring = {'api-version':'3.0'}
 headers = {
     'X-RapidAPI-Key': API_KEY,
-    'X-RapidAPI-Host': "microsoft-translator-text.p.rapidapi.com",
-    }
+    'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
+    'accept-encoding': 'identity'
+}
+resp = requests.get(url, params=querystring, headers=headers)
+langs = json.loads(resp.text)["translation"]
+print(langs)
 
-conn.request("GET", "/languages?api-version=3.0", headers=headers)
 
-res = conn.getresponse()
-data = json.loads(res.read().decode('utf-8'))
+# import http.client
+# import json
 
-print(data['translation'])
+# conn = http.client.HTTPSConnection("microsoft-translator-text.p.rapidapi.com")
+
+# headers = {
+#     'X-RapidAPI-Key': API_KEY,
+#     'X-RapidAPI-Host': "microsoft-translator-text.p.rapidapi.com",
+#     }
+
+# conn.request("GET", "/languages?api-version=3.0", headers=headers)
+
+# res = conn.getresponse()
+# data = json.loads(res.read().decode('utf-8'))
+
+# print(data['translation'])
+
+"""
+resp.text when not subscribed
+{"message":"You are not subscribed to this API."}
+{"message":"You are not subscribed to this API."}
+"""
