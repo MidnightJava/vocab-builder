3c06c599966f51c80b27df83e394065221ad0e87
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2022-12-04 23:29:06 -0500

    Add vocab

--- vocab_builder/ms_translater_client.py
+++ vocab_builder/ms_translater_client.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import re
 import sys
 import json
 import uuid
@@ -27,9 +28,12 @@ class MSTranslatorClient(TranslatorClient):
     def get_languages(self):
         path = '/languages'
         url = endpoint + path
-        request = requests.get(url, params=params, headers=headers)
-        response = request.json()
-        return response['translation']
+        try:
+            request = requests.get(url, params=params, headers=headers, timeout=5.0)
+            response = request.json()
+            return response['translation']
+        except:
+            return None
 
     def detect_language(self, text):
         path = '/detect?api-version=3.0'
@@ -37,25 +41,32 @@ class MSTranslatorClient(TranslatorClient):
         body = [{
             "text": "'" + text + "'"
         }]
-        request = requests.post(url, headers=headers, json=body)
-        response = request.json()
-        print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
-        return response
+        try:
+            request = requests.post(url, headers=headers, json=body, timeout=5.0)
+            response = request.json()
+            # print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
+            return response
+        except:
+            return None
     
-    def translate(self, text):
+    def translate(self, from_lang, to_lang, text):
         path = '/translate?api-version=3.0'
         url = endpoint + path
         params = {
-            "from": "it",
-            "to": "en"
+            "from": from_lang,
+            "to": to_lang
         }
         body = [{
             "text": "'" + text + "'"
         }]
-        request = requests.post(url, params=params, headers=headers, json=body)
-        response = request.json()
-        print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
-        return response
+        try:
+            request = requests.post(url, params=params, headers=headers, json=body, timeout=5.0)
+            response = request.json()
+            # print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
+            text = response[0]["translations"][0]["text"]
+            return re.sub("^'", "", re.sub("'$", "", text))
+        except:
+            return None
 
 """
 resp.text when not subscribed
--- vocab_builder/vocab_builder.py
+++ vocab_builder/vocab_builder.py
@@ -3,6 +3,8 @@ load_dotenv()
 
 import json
 from os.path import exists, sep
+import sys
+from collections import defaultdict
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from ms_translater_client import MSTranslatorClient
 
@@ -11,10 +13,17 @@ DATA_DIR = "data"
 class VocabBuilder():
 
     def __init__(self, **kwargs):
-        self.vocab_filename = f"{DATA_DIR}{sep}{kwargs['from_lang']}_{kwargs['to_lang']}_vocab"
+        self.from_lang = kwargs["from_lang"]
+        self.to_lang = kwargs["to_lang"]
+        self.vocab_filename = f"{DATA_DIR}{sep}{kwargs['to_lang']}_{kwargs['from_lang']}_vocab"
         self.initialize_vocab()
         self.client = MSTranslatorClient()
-        self.langs = self.client.get_languages()
+        if not kwargs.get("no_word_lookup"):
+            langs = self.client.get_languages()
+            self.langs = langs if langs else {}
+            self.check_langs()
+        else:
+            self.langs = {}
         
         if kwargs.get("pr_avail_langs", None):
             print("Available languages for translation")
@@ -23,24 +32,65 @@ class VocabBuilder():
             for k,v in self.langs.items():
                 print(f"{k}\t{v['name']}")
         elif kwargs.get("pr_word_cnt", None):
-            words = self.get_vocab()
-            print(f"{len(words)} words saved")
-        else:
-            lang = self.client.detect_language("pomeriggio")
-            print (lang)
+            vocab = self.get_vocab()
+            print(f"{len(vocab)} {self.to_lang} TO {self.from_lang} words saved")
+        elif kwargs.get("add_vocab", None):
+            self.run_add_vocab()
+            
+    def run_add_vocab(self):
+        done = False
+        new_words = []
+        while not done:
+            w_to = input(f"{self.to_lang} word (Enter to quit): ").lower()
+            if not len(w_to):
+                done = True
+            else:
+                if len(self.langs) == 0:
+                    w_from = input(f"{self.from_lang} word: ").lower()
+                    new_words.append((w_from, w_to))
+                else:
+                    w_from = self.client.translate(self.to_lang, self.from_lang, w_to).lower()
+                    if w_from and w_from != w_to:
+                        print(f"translation: {w_from}")
+                        new_words.append((w_from, w_to))
+                    else:
+                        print("No translation found")
+        self.merge_vocab(new_words)
         
-            self.client.translate("pomeriggio")
-    
     def get_vocab(self):
         with open(self.vocab_filename, 'r') as f:
             contents = f.read()
-            return json.loads(contents)
+            return  defaultdict(list, json.loads(contents))
+        
+    def set_vocab(self, vocab):
+        with open(self.vocab_filename, 'w') as f:
+            f.write(json.dumps(vocab))
+            
+    def merge_vocab(self, new_words):
+        vocab = self.get_vocab()
+        for w_from, w_to in new_words:
+           vocab[w_to] = w_from
+        self.set_vocab(vocab)
     
     def initialize_vocab(self):
-        
         if not exists(self.vocab_filename):
             with open(self.vocab_filename, 'a') as f:
                 f.write(json.dumps({}))
+                
+    def check_langs(self):
+        found_from = found_to = False
+        for k,v in self.langs.items():
+            if k == self.from_lang:
+                found_from = True
+                self.from_langname = v["name"]
+            elif k == self.to_lang:
+                found_to = True
+                self.to_langname = v["name"]
+            if found_from and found_to: break
+        if not found_from or not found_to:
+            missing = (self.from_lang if not found_from else "") + (f" {self.to_lang}" if not found_to else "")
+            print(f"One or both anguages specified are not available: {missing}")
+            sys.exit(0)
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="Vocab Builder", add_help=False,
@@ -63,22 +113,22 @@ if __name__ == "__main__":
                          help="Give the translation of words instead of relying on an external translation service")
     
     testGroup = parser.add_argument_group(title = "Test Vocabulary Options")
-    testGroup.add_argument("-cct", "--correct-count-threshold", type=int, default=5, metavar="INTEGER",
+    testGroup.add_argument("-cct", "--min-correct", type=int, default=5,
                          help="Prioritize testing of words that have not been answered correctly this many times in a row")
-    testGroup.add_argument("-at", "--age-threshold", type=int, default=15, metavar="INTEGER",
+    testGroup.add_argument("-at", "--min-age", type=int, default=15,
                          help="Prioritize testing of words that have not been tested for at least this many days")
     
     othGroup = parser.add_argument_group(title = "Other Options")
-    othGroup.add_argument("-fl", "--from-lang", default="en", metavar="XY",
-                         help="ID code for the language you know (use --pal to list them). Used only if the --no-word-lookup option is not selected")
-    othGroup.add_argument("-tl", "--to-lang", default="es", metavar="XY",
-                         help="ID code for the language you're learning (use --pal to list them). Used only if the --no-word-lookup option is not selected")
+    othGroup.add_argument("-fl", "--from-lang", default="en",
+                         help="Name of the language you know. You must use one of the ID codes displayed with the --pal option unless--no-word-lookup option is selected")
+    othGroup.add_argument("-tl", "--to-lang", default="it",
+                         help="Name of the language you're learning. You must use one of the ID codes displayed with the --pal option unless--no-word-lookup option is selected")
     args = parser.parse_args()
     VocabBuilder(add_vocab = args.add_vocab,
                  test_vocab = args.test_vocab,
                  no_word_lookup = args.no_word_lookup,
-                 correct_count_threshold = args.correct_count_threshold,
-                 age_threshold = args.age_threshold,
+                 min_correct = args.min_correct,
+                 min_age = args.min_age,
                  pr_word_cnt = args.pr_word_cnt,
                  pr_avail_langs = args.pr_avail_langs,
                  from_lang = args.from_lang,
